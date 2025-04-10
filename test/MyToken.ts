b08957a35883335a228cfd5cfc3071add867884b
Junseok Shin <0323shin@naver.com>
2025-04-10 19:32:29 +0900

    transfer emit test

--- test/MyToken.ts
+++ test/MyToken.ts
@@ -45,16 +45,24 @@ describe("My Token", () => {
   });
   describe("Transfer", () => {
     it("should have 0.5MT", async () => {
+      const signer0 = signers[0];
       const signer1 = signers[1];
-      await myTokenC.transfer(
-        hre.ethers.parseUnits("0.5", decimals),
-        signer1.address
-      );
+      await expect(
+        myTokenC.transfer(
+          hre.ethers.parseUnits("0.5", decimals),
+          signer1.address
+        )
+      )
+        .to.emit(myTokenC, "Transfer")
+        .withArgs(
+          signer0.address,
+          signer1.address,
+          hre.ethers.parseUnits("0.5", decimals)
+        );
       expect(await myTokenC.balanceOf(signer1.address)).equal(
         hre.ethers.parseUnits("0.5", decimals)
       );
     });
-    //수수료는 냈지만 실제 balance는 전송하지 못했다.
     it("should be reverted with insufficient balance error", async () => {
       const signer1 = signers[1];
       await expect(
