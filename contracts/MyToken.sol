b24fdb242072e50c04e4f4b2c096aff641ff4731
Junseok Shin <0323shin@naver.com>
2025-04-10 19:32:47 +0900

    added approve and transferForm

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -2,12 +2,16 @@
 pragma solidity ^0.8.28;
 
 contract MyToken {
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed spender, uint256 amount);
+
     string public name;
     string public symbol;
     uint8 public decimals;
 
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) allowance;
 
     constructor(
         string memory _name,
@@ -23,9 +27,26 @@ contract MyToken {
         _mint(_amount * 10 ** uint256(decimals), msg.sender); // 1 MT
     }
 
+    function approve(address spender, uint256 amount) external {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(spender, amount);
+    }
+
+    function transferFrom(address from, address to, uint256 amount) external {
+        address spender = msg.sender;
+        require(allowance[from][spender] >= amount, "insufficient allowance");
+        allowance[from][spender] -= amount;
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount;
+        emit Transfer(from, to, amount);
+    }
+
     function _mint(uint256 amount, address owner) internal {
         totalSupply += amount;
         balanceOf[owner] += amount;
+
+        // 무에서 화페를 발행시켜서 owner에게 줬다
+        emit Transfer(address(0), owner, amount);
     }
 
     function transfer(uint256 amount, address to) external {
@@ -33,5 +54,19 @@ contract MyToken {
 
         balanceOf[msg.sender] -= amount;
         balanceOf[to] += amount;
+
+        emit Transfer(msg.sender, to, amount);
     }
 }
+
+/*
+approve(다른 사람이 내 token을 보내게 한다 => 아주*n 신뢰할만한 곳에만 전달)
+ - allow spender address to send my token
+transferFrom(나의 token을 다른 주소로 보낸다)
+ - spender: owner -> target address
+
+* token owner --> bank contract
+* token owner --> router contract --> bank contract
+* token owner --> router contract --> bank contract(multi contract)
+
+*/
